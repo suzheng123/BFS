cc7f5fc2a4baef1783bebea5e119319111551e6f
suzheng123 <suzheng@ucsc.edu>
2019-05-08 21:33:53 -0700

    commit

---
+++ n_aryTreeLevel.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        
+        stack = [root]
+        tree = []
+        while stack:
+            level =  []
+            temp_stack = []
+            for node in stack:
+                level.append(node.val)                
+                for child in node.children:
+                    temp_stack.append(child)
+            stack = temp_stack            
+            tree.append(level)
+        
+        return tree
