8c333b35c1d1f69ffaf1308807f8b434fbcd4557
suzheng123 <suzheng@ucsc.edu>
2019-05-08 21:36:35 -0700

    commit
    in order traversal

---
+++ inOrderTraversal.py
@@ -0,0 +1,20 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Inorder in ArrayList which contains node values.
+    """
+   
+    def inorderTraversal(self, root):
+        # write your code here
+        if not root:
+            return []
+            
+        return self.inorderTraversal(root.left) + [root.val]\
+            + self.inorderTraversal(root.right)
